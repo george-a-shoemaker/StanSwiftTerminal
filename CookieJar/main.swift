61d3ce51e4547ba400f25f1b15f4e06dc1e31649
George Shoemaker <george.a.shoemaker@gmail.com>
2024-05-17 22:32:30 -0700

    Simplify cookie kludge

--- CookieJar/main.swift
+++ CookieJar/main.swift
@@ -7,30 +7,26 @@
 
 import Foundation
 
-
-var cookieTot : Int = 0
+// Notice that a CookieJar has a quantity func
+// The quantity is "state" that changes
+// as cookies are added and removed
 let cookieJarA = CookieJar()
 cookieJarA.insert(cookie: Cookie(flavor: .chocolateChip))
-cookieTot += 1
 cookieJarA.insert(cookie: Cookie(flavor: .stroopwafel))
-cookieTot += 1
 cookieJarA.insert(cookie: Cookie(flavor: .chocolateChip))
-cookieTot += 1
 cookieJarA.insert(cookie: Cookie(flavor: .fortune))
-cookieTot += 1
 print( "cookieJarA Contents: \(cookieJarA.emojis())" )
 print("\nLet's move a cookie from cookieJarA to cookieJarB")
 let cookieJarB = CookieJar()
-while cookieTot > 0 {
+while cookieJarA.quantity() > 0 {
     cookieJarB.insert(
         cookie: cookieJarA.remove()!
         // ! means force unwrap the result of .remove()
     )
-    cookieTot -= 1
 }
 print( "cookieJarA Contents: \(cookieJarA.emojis())" )
 print( "cookieJarB Contents: \(cookieJarB.emojis())" )
-print (cookieTot)
+print (cookieJarA.quantity())
 
 //insertRemoveExample()
 
